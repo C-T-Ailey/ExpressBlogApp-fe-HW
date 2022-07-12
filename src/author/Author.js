a777f3c2792916205cc6a1d829f287c01e82c619
cailey90 <chris.ailey90@gmail.com>
2022-07-12 16:42:58 +0100

    added and modified App.js, index.js, AuthorList.js, Author.js; created AuthorCreateForm.js

---
+++ src/author/Author.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Author(props) {
+  return (
+    <>
+    <td>{props.name}</td>
+    <td>{props.emailAddress}</td>
+    </>
+  )
+}
---
+++ src/author/AuthorList.js
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import Author from './Author'
+import AuthorCreateForm from './AuthorCreateForm'
+
+export default function AuthorList() {
+
+    const [authors, setAuthors] = useState([])
+
+    useEffect(() => {
+        loadAuthorList()
+    }, [])
+
+    const loadAuthorList = () => {
+        axios.get("author/index")
+        .then((response => {
+            console.log(response)
+            setAuthors(response.data.authors)
+        }))
+        .catch((error) => {
+            console.log(error)
+        })
+    }
+
+
+    const allAuthors = authors.map((author, index) => (
+        <tr key={index}>
+            <Author {...author} />
+        </tr>
+    ))
+
+    const addAuthor = (author) => {
+        axios.post("author/add", author)
+        .then(response => {
+            console.log("Author added successfully.")
+            loadAuthorList();
+        })
+        .catch((error) => {
+            console.log("Error adding author!")
+            console.log(error)
+        })
+    }
+
+  return (
+    <div>
+        <h1>Authors</h1>
+        <div>
+            <table>
+                <tbody>
+                    <tr>
+                        <th>Name</th>
+                        <th>Email Address</th>
+                    </tr>
+                    {allAuthors}
+                </tbody>
+            </table>
+        </div>
+    </div>
+  )
+}
